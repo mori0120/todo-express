223ded30b843ec459d75cf61cce4f1e42cd0a8d3
Mori Yasuaki <mori.yasuaki18@gmail.com>
2022-10-14 03:12:17 +0900

    post-request is added

--- app.js
+++ app.js
@@ -5,12 +5,40 @@ let todos = [
     { id : 2, title: '下書き', completed: true }
 ]
 const app  = express()
+
+app.use(express.json())
+
+// ToDo listの取得
 app.get('/api/todos', (req, res) => {
     if(!req.query.completed){
         return res.json(todos)
     }
     const completed = req.query.completed === 'true'
     res.json(todos.filter(todo => todo.completed === completed))
+})
+
+// ToDo ID値を管理する変数
+let id = 2
+
+// ToDo 新規登録
+app.post('/api/todos', (req, res, next)=>{
+    const { title } = req.body
+    if(typeof title !== 'string' || !title){
+        const err = new Error('title is required')
+        err.statusCode = 400
+        return next(err)
+    }
+    // ToDo作成
+    const todo = { id: id+=1, title, completed: false}
+    todos.push(todo)
+    // ステータスコード201を返す
+    res.status(201).json(todo)
+})
+
+// エラーハンドリングミドルウェア
+app.use((err, req, res, next)=>{
+    console.error(err)
+    res.status(err.statusCode || 500).json({ error: err.message })
 })
 
 
